103edddd1aaa7ab31ba82b3bfafbff09eb5a45c1
Andreas Wachter <andreas.wachter@buddyspencer.monster>
2026-04-20 11:24:49 +0200

    Update site URL and base URL in config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,10 +13,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://cooperspencer.github.io/',
+  url: 'https://gickup.dev/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/gickup-documentation/',
+  baseUrl: '',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
